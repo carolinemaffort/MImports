1c4f97c7cc62fa17051729e19eca3ec279140781
carolinemaffort <92541410+carolinemaffort@users.noreply.github.com>
2022-10-24 21:05:13 -0300

    Add files via upload

--- resources/configure.php
+++ resources/configure.php
@@ -24,20 +24,20 @@ if ( $_SERVER ['HTTP_HOST'] == 'localhost' ){ // Em localhost
     $confDB['pass']     = "";
     $confDB['schema']   = "mimports";
 }else{                                  // Em deploy
-    $confDB['host']  = "";
-    $confDB['user']  = "";
-    $confDB['pass']  = "";
-    $confDB['schema']= "";
+    $confDB['host']  = "51.79.72.47";
+    $confDB['user']  = "hostdeprojetos_mimports";
+    $confDB['pass']  = "pUBN2?U2{;H?";
+    $confDB['schema']= "hostdeprojetos_mimports";
 }
 $_SESSION["confDB"] = $confDB;
 
 // Definir Configurações de SMTP
-$smtp['host']       = 'smtp.gmail.com';
-$smtp['auth']       =  true;
-$smtp['secure']     = 'tls';
-$smtp['username']   = 'maffort.imports@gmail.com';
-$smtp['password']   = 'xoqxctxrvzlslwoe';
-$smtp['port']       =  465;
-$_SESSION["smtp"]   = $smtp;
+// $smtp['host']       = 'smtp.gmail.com';
+// $smtp['auth']       =  true;
+// $smtp['secure']     = 'tls';
+// $smtp['username']   = 'maffort.imports@gmail.com';
+// $smtp['password']   = 'xoqxctxrvzlslwoe';
+// $smtp['port']       =  465;
+// $_SESSION["smtp"]   = $smtp;
 
 ?>
