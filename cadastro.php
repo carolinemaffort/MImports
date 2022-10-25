5df0f8c1c257f5a5f96c612505b7e691126f4912
carolinemaffort <92541410+carolinemaffort@users.noreply.github.com>
2022-10-24 21:23:08 -0300

    Add files via upload

--- cadastro.php
+++ cadastro.php
@@ -1,8 +1,8 @@
 <?php
 
-use email\Email;
+// use email\Email;
 
-require __DIR__.'\\class\\Email.class.php';
+// require __DIR__.'\\class\\Email.class.php';
 
 // Requer o arquivo de autoloading
 require_once(__DIR__ . "\\etc\\autoload.php");
@@ -30,12 +30,35 @@ $result = $connection->query($dao->createQuery());
 // Printa o resultado
 echo "Query de inserção: " . $result . "<br>";
 
+function enviarEmail( $to, $from, $replyTo, $subject, $message ) {
+    $headers =  'From: '       .$from .    "\r\n" .
+                'Reply-To: '   .$replyTo . "\r\n" .
+                'X-Mailer: PHP/' . phpversion();
+    $mail_enviado =   mail($to, $subject, $message, $headers);
+    // A função mail() retorna true ou false pro envio
+    if ( $mail_enviado ){
+        echo ("Email enviado com sucesso!");
+        return (true);
+    }else{
+        echo ("Falha ao Enviar Email! \n<br> Verifique suas configurações smtp no php.ini");
+        return (false);
+    }
+    
+}
 
-$emailer = new Email();
-$emailer->setFrom("maffort.imports@gmail.com", "Maffort Imports");
-$emailer->addTo("$email", "$nome");
-$emailer->setSubject("Teste de Email");
-$emailer->setMsgTxt("Testandoooo");
-$emailer->send_gmail();
+$mailTo      = $email; // Destinário do Email
+$mailFrom    = 'maffort.imports@gmail.com'; // Remetente do Email
+$mailReplyTo = 'naoresponda@gmail.com';  // Em caso de Resposta enviar pra quem?
+$mailSubject = 'Título do Email';       // Título do Email
+$mailMessage = 'Corpo do Email';        // Corpo do Email
+
+enviarEmail($mailTo, $mailFrom, $mailReplyTo, $mailSubject, $mailMessage);
+
+#$emailer = new Email();
+#$emailer->setFrom("maffort.imports@gmail.com", "Maffort Imports");
+#$emailer->addTo("$email", "$nome");
+#$emailer->setSubject("Teste de Email");
+#$emailer->setMsgTxt("Testandoooo");
+#$emailer->send_gmail();
 
 ?>
