ce3c9d8d58f025afe9f5c41980982a8834514d61
carolinemaffort <92541410+carolinemaffort@users.noreply.github.com>
2022-11-28 17:58:05 -0300

    Update inserir.php

--- crud/inserir.php
+++ crud/inserir.php
@@ -23,27 +23,28 @@ include ('../class/dao/InserirDao.class.php');
 include ('../class/connection/DatabaseConnection.class.php');
 
 // Recebe os dados do formulário
-$tmpName = $_FILES['foto']['tmp_name'];echo $tmpName;
-//$time = (new \DateTime())->format('YmdHisu');
-$pathFile = '../assets/images/' . $_FILES['foto']['name'];echo $pathFile;
-move_uploaded_file($tmpName, $pathFile);echo "mudou";
+print_r($_FILES['foto']);
+$tmpName = $_FILES['foto']['tmp_name'];
+$time = (new DateTime())->format('YmdHisu');
+$pathFile = '../assets/images/' . $time .  $_FILES['foto']['name'];
+move_uploaded_file($tmpName, $pathFile);
 $titulo = $_REQUEST["titulo"];
 $preco = $_REQUEST["preco"];
 $descricao = $_REQUEST["desc"];
 
 
 // Transporta os valores do formulário para o model de cadastro
-$model = new InserirModel(0, $_FILES['foto']['name'],  $titulo, $preco, $descricao);
+$model = new InserirModel(0, $time . $_FILES['foto']['name'],  $titulo, $preco, $descricao);
 
 //print_r($model);
 
 // Cria um DAO para formular queries com o Id, Email e Nome pegos do model
 $dao = new InserirDao(array($model->getId(), $model->getFoto(), $model->getTitulo(), $model->getPreco(), $model->getDescricao()));
 print_r($dao);
-echo "vai fazer";
+
 // Cria uma nova conexão com os dados do conf.php
 $connection = new DatabaseConnection();
-echo "fez";
+
 // Fazer uma query de criar um Cadastro no banco de dados
 $result = $connection->query($dao->createQuery());
 
