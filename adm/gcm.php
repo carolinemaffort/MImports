95ca52d7b370d8a74f2cc974cba9e5a2ec7df4cf
bybiamoraes <92543442+bybiamoraes@users.noreply.github.com>
2022-12-01 17:51:10 -0300

    Update gcm.php

--- adm/gcm.php
+++ adm/gcm.php
@@ -43,17 +43,23 @@ include ('../class/connection/DatabaseConnection.class.php');
     
 <div class="row">
 
-        <div class="col-md-4">
+        <div class="col-md-12">
+        
         <div class="section-heading">
               <h2>Gerenciamento</h2>
         </div>
               <h4>O que você deseja fazer?</h4>
              <br>
        
-        <form action="../crud/inserir.html" class="needs-validation" novalidate>
+        <form action="../crud/inserir.html" class="needs-validation" novalidate style="display:inline-block">
                 <button type="submit" class="btn btn-danger filled-button">Inserir</button>
-        </form><br>
-        <button type="submit" class="btn btn-danger filled-button"><a href="logout.php">Logout</a> </button><br>
+        </form>
+        <a class="btn btn-danger filled-button" href="#feedbacks" style="display:inline-block">Feedbacks</a>
+        <a class="btn btn-danger filled-button" href="logout.php" style="display:inline-block">Logout</a><br>
+
+        
+        <div class="row">
+
         <?php
         try {
               $connection = new DatabaseConnection();
@@ -63,13 +69,13 @@ include ('../class/connection/DatabaseConnection.class.php');
               // print_r($result);
 
               foreach ($result as $rere) {
-                echo "<form name='formulario" . $rere["id"] . "' action='../crud/alterar.php' method='post' enctype='multipart/form-data' style='display: inline-block'><br> - ID: " . $rere["id"] . " <br>
+                echo "<div class='col-md-4'> <form name='formulario" . $rere["id"] . "' action='../crud/alterar.php' method='post' enctype='multipart/form-data' style='display: inline-block'><br> - ID: " . $rere["id"] . " <br>
                 - Foto: <br> <img src='https://hostdeprojetosdoifsp.gru.br/mimports/assets/images/" . $rere["foto"] . "' alt='' width='300'><br><br><input type='file' name='foto' value='" . $rere["foto"] . "'><br>
                 <br>- Titulo: <input type='text' name='titulo' value='" . $rere["titulo"] . "'> 
                 <br>- Descrição: <input type='text' name='desc' value='" . $rere["descricao"] . "'> 
                 <br> - Preço: <input type='text' name='preco' value='" . $rere["preco"] . "'><input type='hidden' name='id' value='" . $rere['id'] . "'></form>" . "<br>" ."<br>" .
                 "<button type='submit' class='btn btn-danger filled-button' onclick='altform(" . $rere["id"] . ")'>Alterar</button>" .  "&nbsp" . 
-                "<form action='../crud/excluir.php' method='post' style='display: inline-block'>  <button type='submit' class='btn btn-danger filled-button' >Deletar</button> <input type='hidden' name='id' value='" . $rere['id'] . "'></form>" . "<br><br>";
+                "<form action='../crud/excluir.php' method='post' style='display: inline-block'>  <button type='submit' class='btn btn-danger filled-button' >Deletar</button> <input type='hidden' name='id' value='" . $rere['id'] . "'></form> " . "<br><br></div>";
              }
              
              $connection->close();
@@ -80,6 +86,37 @@ include ('../class/connection/DatabaseConnection.class.php');
         }
 
         ?>
+        
+
+  <div class="col-md-12" id="feedbacks">
+          <div class="section-heading">
+              <h2>Feedbacks</h2>
+          </div>
+
+          <?php
+          $connection = new DatabaseConnection();
+              $sql = "SELECT id, nome, email, mensagem FROM cadastro";
+              $resultado = $connection->query($sql); 
+
+              //print_r($result);
+
+
+              foreach ($resultado as $re){
+                echo "<div class='col-lg-4 col-md-4 all gra'>
+              <div class='product-item'>
+                
+                <div class='down-content'>
+                  <h4> " . $re["nome"] . " </h4>
+                  <p> " . $re["mensagem"] . " </p>
+                  <form action='../crud/fdexcluir.php' method='post' style='display: inline-block'>  <button type='submit' class='btn btn-danger filled-button' >Deletar</button> <input type='hidden' name='id' value='" . $re['id'] . "'></form>
+                </div>
+              </div>
+            </div>";
+            }
+?>
+   </div>
+
+        </div>
           </div>
 </div>
     </div>
@@ -91,6 +128,8 @@ include ('../class/connection/DatabaseConnection.class.php');
           }
 
         </script>
+
+
 
 </body>
 </html>
