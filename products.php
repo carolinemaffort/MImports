d4f4350b590818dbc76ed5764531fb1ee0090cc2
bybiamoraes <92543442+bybiamoraes@users.noreply.github.com>
2022-12-02 15:09:53 -0300

    Update products.php

--- products.php
+++ products.php
@@ -128,7 +128,10 @@ https://templatemo.com/tm-546-sixteen-clothing
                     $connection = new DatabaseConnection();
                     $sql = "SELECT id, foto, titulo, descricao, preco FROM produtos";
                     $resultado = $connection->query($sql); 
-                
+                    $favoritos = json_decode($_COOKIE["favoritos"]);
+                    if (!is_array($favoritos)) {
+                      $favoritos = array();
+                    }
                 
                     foreach ($resultado as $re){
                         echo "<div class='col-lg-4 col-md-4 all gra'>
@@ -140,9 +143,13 @@ https://templatemo.com/tm-546-sixteen-clothing
                           <p> " . $re["descricao"] . " </p>
                           <ul class='stars'>
 
-                            <li><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-heart' viewBox='0 0 16 16'>
+                            <li" . (is_int(array_search($re["id"], $favoritos)) ? "hidden" : "") . "><a href='favoritos.php?id=" . $re["id"] . "' style='color:black'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-heart' viewBox='0 0 16 16'>
                               <path d='m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z'/>
-                            </svg></li>
+                            </svg></a></li>
+
+                            <li" . (is_int(array_search($re["id"], $favoritos)) ? "" : "hidden") . "><a href='desfavoritos.php?id=" . $re["id"] . "' style='color:red'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-heart-fill' viewBox='0 0 16 16'>
+                            <path fill-rule='evenodd' d='M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z'/>
+                          </svg></a></li>
           
                             
                           </ul>
