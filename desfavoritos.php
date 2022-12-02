a8d28084dff273769da1e0991a116a11009dad2c
bybiamoraes <92543442+bybiamoraes@users.noreply.github.com>
2022-12-02 19:50:55 -0300

    Add files via upload

---
+++ desfavoritos.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once(__DIR__ . "/etc/autoload.php");
+
+$favoritos = json_decode($_COOKIE["favoritos"]);
+
+if (!is_array($favoritos)) {
+  $favoritos = array();
+}
+
+$index = array_search($_REQUEST["id"], $favoritos);
+if (is_int($index)) {
+  unset($favoritos[$index]);
+  if (setcookie("favoritos", json_encode($favoritos), time() + 60 * 60 * 24 * 3)) {
+    echo "Favoritando...";
+  } else {
+    echo "Deu ruim! ";
+  }
+}
+?> 
+
+<script>
+  //window.location.replace("https://localhost/mimports/adm/gcm.php"); 
+   window.location.replace("https://<?= $_SERVER["SERVER_NAME"] ?>/mimports/products.php"); 
+</script>
